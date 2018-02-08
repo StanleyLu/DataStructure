048cffdd6f743f78e7dbf85d3cb7f50fc4d39c37
StanleyLu <x8892965@yahoo.com.tw>
2018-02-08 15:43:51 +0800

    合併排序法
    合併排序做法

--- Merge_Sort_Example.py
+++ Merge_Sort_Example.py
@@ -10,11 +10,8 @@ while s < 9:
 	insertArray.append(tempRandNum)
 	s = s + 1 
 
-# insertArray = [5,6,8,4,2,9,3,7,1]
-# insertArray = [5,6,8,4,4,2,3,9,3,7,1]
-# insertArray = [5,1,4,2,8]
 print(insertArray)
-# sortedList = []
+
 def mergeList(leftList, rightList):
 
 	if len(leftList) == 0:  
@@ -51,53 +48,9 @@ def chopList(sourceList):
 	return mergeList(leftData, rightData)
 
 
-# print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-# print(chopList(insertArray))
-# print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-
-# 第二次做
-
-def mergeList(leftList, rightList):
-
-	if len(leftList) == 0:
-		return rightList
-	elif len(rightList) == 0:
-		return leftList
-	elif leftList[0] < rightList[0]:
-		return [leftList[0]]+ mergeList(leftList[1:], rightList)
-	else:
-		# return [rightList[0]] + mergeList(rightList[1:], leftList)
-		return [rightList[0]] + mergeList(leftList, rightList[1:])
-
-
-
-def chopList(sourceList):
-	
-	if len(sourceList) <= 1:
-		return sourceList
-
-	centerKey = len(sourceList)/2
-
-	leftList = sourceList[:centerKey]
-	rightList = sourceList[centerKey:]
-	
-	leftData = chopList(leftList)
-	leftData = chopList(rightList)
-
-	return mergeList(chopList(leftList), chopList(rightList))
-
-
-
 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 print(chopList(insertArray))
 print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-
-
-
-
-first = [0]
-second = [1]
-print(first + second)
 
 
 
